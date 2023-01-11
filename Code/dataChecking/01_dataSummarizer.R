bf7ff5eeaacc6a70f98410af6d338854382bd942
RobLiddell <116843483+RobLiddell@users.noreply.github.com>
2023-01-11 15:07:19 -0500

    load data types through a loop

--- Code/dataChecking/01_dataSummarizer.R
+++ Code/dataChecking/01_dataSummarizer.R
@@ -277,34 +277,7 @@ if(FALSE){
   tibble(varnm=colnames(data)) %>% 
     mutate(row=row_number())
 
-  shortcatsCheckBoxFormatting <- function(tp,varnm,values,shortcats){
-    if(tp=='01'){
-      variableValue=str_extract(varnm,'(?<=[_]{3})[0-9]+')
-      variableLabelIndex=which(values==variableValue)
-      return(shortcats[variableLabelIndex])
-    }
-    return(shortcats)
-  }
   
-  valueCheckBoxFormatting <- function(tp,varnm,values){
-    if(tp=='01'){
-      return(str_extract(varnm,'(?<=[_]{3})[0-9]+'))
-    }
-    return(values)
-  }
-    
-  
-  dataTypeChanging <- dataDict %>% 
-    select(varnm,tp,values,shortcats) %>% 
-    mutate(values=str_remove_all(values,'\"') %>% str_split(','),
-           shortcats=str_remove_all(shortcats,'\"') %>% str_split(',')) %>%
-    mutate(varnm=if_else(tp=='01',map2(varnm,values,function(.x,.y) (paste0(.x,'___',.y))),map(varnm,function(.x) (.x)))) %>% 
-    unnest_longer(varnm) %>% 
-    left_join(tibble(varnm=colnames(data)) %>% 
-                mutate(row=row_number())) %>%
-    # filter(str_detect(varnm,'cl_mets')) %>% 
-    mutate(shortcats=pmap(list(tp,varnm,values,shortcats),~shortcatsCheckBoxFormatting(..1,..2,..3,..4)),
-           values=pmap(list(tp,varnm,values),~valueCheckBoxFormatting(..1,..2,..3))) 
   
   
   dataTypeChanging %$%
@@ -387,6 +360,73 @@ dataDict %>%
 }
 
 
+####Formatting Full Table####
 
+if(FALSE){
+  
+  
+  shortcatsCheckBoxFormatting <- function(tp,varnm,values,shortcats){
+    if(tp=='01'){
+      variableValue=str_extract(varnm,'(?<=[_]{3})[0-9]+')
+      variableLabelIndex=which(values==variableValue)
+      return(shortcats[variableLabelIndex])
+    }
+    return(shortcats)
+  }
+  
+  valueCheckBoxFormatting <- function(tp,varnm,values){
+    if(tp=='01'){
+      return(str_extract(varnm,'(?<=[_]{3})[0-9]+'))
+    }
+    return(values)
+  }
+  
+  
+  dataTypeChanging <- dataDict %>% 
+    select(varnm,tp,values,shortcats) %>% 
+    mutate(values=str_remove_all(values,'\"') %>% str_split(','),
+           shortcats=str_remove_all(shortcats,'\"') %>% str_split(',')) %>%
+    mutate(varnm=if_else(tp=='01',map2(varnm,values,function(.x,.y) (paste0(.x,'___',.y))),map(varnm,function(.x) (.x)))) %>% 
+    unnest_longer(varnm) %>% 
+    left_join(tibble(varnm=colnames(data)) %>% 
+                mutate(row=row_number())) %>%
+    # filter(str_detect(varnm,'cl_mets')) %>% 
+    mutate(shortcats=pmap(list(tp,varnm,values,shortcats),~shortcatsCheckBoxFormatting(..1,..2,..3,..4)),
+           values=pmap(list(tp,varnm,values),~valueCheckBoxFormatting(..1,..2,..3))) 
+  
+  
+  
+  
+  loadedData <- data %>% 
+    select(ord,starts_with('redcap_'))
+  
+  for(i in 1:nrow(dataTypeChanging)){
+    currentVariable=dataTypeChanging$varnm[[i]]
+    currentDataType=dataTypeChanging$tp[[i]]
+    currentDataValues=dataTypeChanging$values[[i]]
+    currentDataLabels=dataTypeChanging$shortcats[[i]]
+    
+    currentData <- data[[currentVariable]]
+    
+    
+    currentData <- switch (currentDataType,
+                           `$` = currentData %>% as.character() %>% str_wrap(20) %>% factor(exclude=c('',NA)),
+                           `01` =currentData %>% factor(levels=c(1,0),labels=c(currentDataLabels,'0')),
+                           `##` = currentData %>% as.numeric(),
+                           `#` = currentData %>%  as.integer(),
+                           `dt` = currentData %>% ymd() %>% as.integer(),
+                           `b` = currentData %>%  factor(levels=currentDataValues,labels = currentDataLabels) %>% str_wrap(11) %>% fct_rev(),
+                           `b0` = currentData %>% factor(levels=currentDataValues,labels = currentDataValues) %>% fct_rev())
+    loadedData[[currentVariable]] <- currentData
+    
+  }
+  
+  loadedData
+  
+  colnames(data[1])
+  
+  data
+  
+}
 
 
