3423fd071b37dd5bd7bef89a60395bd1b6d167d0
RobLiddell <116843483+RobLiddell@users.noreply.github.com>
2023-01-12 11:44:45 -0500

    improved date formatting capabilaties

--- Code/dataChecking/03 Redcap Data Reader.R
+++ Code/dataChecking/03 Redcap Data Reader.R
@@ -17,33 +17,45 @@ allVariableInfo <- dataDict %>%
   select(varnm,tp,values,shortcats,source,myLabel) %>% 
   unnest_longer(varnm)
 
-createColList <- function(dataType){
+createColList <- function(dataType,variableValues){
     switch (dataType,
-          `$` = 'c',
-          `01` = 'f',
+          `$` = col_character(),
+          `01` = 'i',
           `##` = 'n',
           `#` = 'i',
-          `dt` = 'D',
-          `b` = 'f',
-          `b0` = 'f')
+          `dt` = col_character(),
+          `b` = col_factor(levels = variableValues),
+          `b0` = col_factor(levels = variableValues))
 }
 
 
 columnTypes <- allVariableInfo %>% 
-  select(varnm,tp) %$%
-  map(tp,~createColList(.x)) %>% 
-  unlist() %>% 
-  setNames(dataWType$varnm) %>% 
-  append(list(redcap_repeat_instrument='c',redcap_repeat_instance='i'),after=0) %>% 
-  cols(!!!.)
+  select(values,tp) %$%
+  map2(tp,values,~createColList(.x,.y)) %>% 
+  setNames(allVariableInfo$varnm) %>% 
+  append(list(redcap_repeat_instrument=col_character(),redcap_repeat_instance='i'),after=0)
 
-
-colNames <- dataWType %>% 
-  pull(varnm) %>% 
-  append(c('redcap_repeat_instrument','redcap_repeat_instance'),after=0) 
+colNames <- columnTypes %>% 
+  names()
 
 #Read Data
-data=read_csv('Data/raw/01 ConcordanceStudy_DATA_2022-12-30_1413.csv',col_types=columnTypes,col_select=all_of(colNames))
+data=vroom('Data/raw/01 ConcordanceStudy_DATA_2022-12-30_1413.csv',delim=',',col_types=columnTypes,col_select=all_of(colNames)) %>% 
+  mutate(rowID=row_number(),.before=1) %>% 
+  mutate(vis_id=str_remove(vis_id,'^[0]*'))
+
+data <- allVariableInfo %>% 
+  filter(tp=='dt') %$% 
+  mutate(data,across(.col=all_of(varnm),ymd))
+
+
+#Proper factor formatting
+data %>% 
+  select(all_of(allVariableInfo$varnm[allVariableInfo$tp%in%c('01','b','b0')] )) %>% 
+  colnames()
+
+for(i in c('01','b','b0')){
+  
+}
 
 
 
